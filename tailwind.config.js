ee6e256ee4aa367b0cba02dea2c92ede6594baa3
LukeOkane99 <theofficiallukeokane@gmail.com>
2024-04-28 00:41:13 +0100

    Rebuilding website using tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-bg": "linear-gradient(#30D5C8, #1C1475ff)",
+      },
+      colors: {
+        "color-primary": "white",
+        "color-secondary": "#1C1475ff",
+        "color-third": "#30D5C8",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '20px',
+        md: '20px'
+      },
+    },
+  },
+  plugins: [],
+}
+
